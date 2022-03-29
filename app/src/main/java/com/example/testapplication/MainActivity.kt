b6e3832e7e156821b45dc98a0c4b40b394d199ca
Andrej <andrej.strogonov@inbox.ru>
2022-03-29 21:33:19 +0300

    Добаавление header

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testapplication
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.testapplication.databinding.ActivityMainBinding
@@ -29,7 +30,13 @@ class MainActivity : AppCompatActivity() {
             navigatorView.setNavigationItemSelectedListener {
                 when(it.itemId) {
                     R.id.item1-> Toast.makeText(this@MainActivity,"Item 1",Toast.LENGTH_SHORT).show()
+                    R.id.item2-> Toast.makeText(this@MainActivity,"Item 2",Toast.LENGTH_SHORT).show()
+                    R.id.item3-> Toast.makeText(this@MainActivity,"Item 3",Toast.LENGTH_SHORT).show()
+                    R.id.item11-> Toast.makeText(this@MainActivity,"Item 11",Toast.LENGTH_SHORT).show()
+                    R.id.item22-> Toast.makeText(this@MainActivity,"Item 22",Toast.LENGTH_SHORT).show()
+                    R.id.item33-> Toast.makeText(this@MainActivity,"Item 33",Toast.LENGTH_SHORT).show()
                 }
+                drawer.closeDrawer(GravityCompat.START)
                 true
             }
         }
